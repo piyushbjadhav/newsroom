4c4f2036dd1fead27e85fdec53e8bf260afbf58b
Piyush Jadhav <piyush.jadhav@nyu.edu>
2015-12-06 14:02:53 -0500

    Added todo's

---
+++ crawler/schema.sql
@@ -0,0 +1,10 @@
+# todo :
+
+Create Schema for Storing articles:
+
+Id : Auto increment , primary key , not null
+url: not null , unique , indexed
+title: Not null
+Author:
+published_date: 
+crawled_date: Not null
